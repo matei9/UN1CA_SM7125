fa87248cb7ed17b25e1a6edd1dbc076365caa33a
TestaMic <mateidiac9@gmail.com>
2025-11-29 10:40:19 +0200

    a52q: patches: camera: Fix MIDAS model detection.

--- target/a52q/patches/camera/customize.sh
+++ target/a52q/patches/camera/customize.sh
@@ -14,7 +14,7 @@ DELETE_FROM_WORK_DIR "vendor" "etc/midas"
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/midas" 0 2000 755 "u:object_r:vendor_configs_file:s0"
 
 # Fix MIDAS model detection
-sed -i "s/a73xq/a72q/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+sed -i "s/a73xq/a52q/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 
 # Upgrade singletake blobs
 DELETE_FROM_WORK_DIR "vendor" "etc/singletake"
