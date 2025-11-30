5c108fcaba4e3649d647d0dea32b7bda43f51599
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-11-30 07:50:21 +0000

    platforms: sm7125: init
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

---
+++ platform/sm7125/sff.sh
@@ -0,0 +1,16 @@
+# Copyright (c) 2025 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Qualcomm Snapdragon 720G devices (sm7125)
+
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
+
+# ImageTagger version
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V701
+
+# Always show remaster button in Gallery
+SEC_FLOATING_FEATURE_SAIV_CONFIG_AI_REVITAL_VERSION=1.7,1
