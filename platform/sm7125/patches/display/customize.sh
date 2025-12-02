16bc626fd049c26aacaa0d746b5db3b7add2a3a0
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-12-02 11:54:47 +0800

    sm7125: display: apply hex patch for libsdmutils.so
    Change-Id: Iac73e0dc81fd76eb88ab1f386853f1fc7f3d5109
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- platform/sm7125/patches/display/customize.sh
+++ platform/sm7125/patches/display/customize.sh
@@ -58,3 +58,7 @@ ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@4.0
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapperextensions@1.0.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapperextensions@1.1.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
 LOG_STEP_OUT
+
+LOG_STEP_IN "- Applying hex patch for libsdmutils"
+HEX_PATCH "$WORK_DIR/vendor/lib64/libsdmutils.so" "40F9F303012A3401" "40F9130080523401"
+LOG_STEP_OUT
