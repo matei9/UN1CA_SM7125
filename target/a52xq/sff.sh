544760d9595b20f5593afd44e717c311617e7a8c
TestaMic <mateidiac9@gmail.com>
2026-04-27 22:40:24 +0300

    target: a52xq: init; platform: sm7225: init.
    Both the device tree (target) and the platform code are based on the Galaxy A52s (a52sxq) and the Snapdragon 778G (sm7325). Sorry for dumping everything in one big commit, beacause it’s still a work in progress. I just wanted to push it now so I can continue working on it later.

---
+++ target/a52xq/sff.sh
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy A52 5G (a52xq)
+
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
+
+# Always show remaster button in Gallery
+SEC_FLOATING_FEATURE_SAIV_CONFIG_AI_REVITAL_VERSION=1.7,1
