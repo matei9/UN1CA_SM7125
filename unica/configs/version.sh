04a408e4d7a3c81dce55e09a6488483bcfd29dbf
TestaMic <mateidiac9@gmail.com>
2026-07-14 19:11:14 +0300

    unica: configs: version: Add ROM Codename.

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -10,6 +10,7 @@ VERSION_PATCH=0
 #
 # Version name
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+ROM_CODENAME="Synapse"
 # Append "+" to version name if commits have been added since the last tag
 LATEST_TAG="$(git describe --tags --abbrev=0 2> /dev/null)"
 if [ "$LATEST_TAG" ]; then
